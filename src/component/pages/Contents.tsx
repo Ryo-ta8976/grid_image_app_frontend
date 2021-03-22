15cb49671fbd889a2bdcd1487b607e1655774c5d
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-03-22 22:07:36 +0900

    add/delete処理まで

--- src/component/pages/Contents.tsx
+++ src/component/pages/Contents.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import GridImageList from '../Organisms/GridImageList'
 import Header from '../Organisms/Header'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Contents(): JSX.Element {
+  const [images, setImages] = useState([{'label': '', 'url': '', 'id': 0}])
+  useEffect(() => {
+    axios.get('/images')
+      .then((response) => {
+        console.log(response.data)
+        setImages(response.data)
+      }).catch(error => console.log(error))
+  }, [])
+
   return (
     <div>
       <Header />
       <div>
-        <GridImageList />
+        <GridImageList images={images} />
       </div>
     </div>
   )

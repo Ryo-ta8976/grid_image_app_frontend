8ad583514f41da288582afda950956ea7d8283bc
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-04-04 11:45:17 +0900

    searchbox追加

--- src/component/Molecules/SearchBox.tsx
+++ src/component/Molecules/SearchBox.tsx
@@ -11,6 +11,7 @@ interface Props {
   paper_width: string;
   place_holder: string;
   display: string;
+  marginRight: string;
 }
 
 export default function CustomizedInputBase(props: Props): JSX.Element {
@@ -34,7 +35,7 @@ export default function CustomizedInputBase(props: Props): JSX.Element {
   return (
     <Paper
       component="form"
-      sx={{ padding: '2px 4px', display: props.display, alignItems: 'center', width: parseInt(props.paper_width) }}
+      sx={{ mr: props.marginRight, padding: '2px 4px', display: props.display, alignItems: 'center', width: parseInt(props.paper_width) }}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
--- src/component/Organisms/Header.tsx
+++ src/component/Organisms/Header.tsx
@@ -11,9 +11,10 @@ export default function Header(): JSX.Element {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar sx={{backgroundColor:"skyblue"}} position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'inline-block', width: '50px' }}>
             My Unspush
           </Typography>
+          <SearchBox marginRight="10px" paper_width='auto' display="inline-block" place_holder="search label" />
           <BasicModal color='primary' index={0} type='add' button_message='Add a phote' title='Add a new photo' label={['Label', 'Photo URL']} submit_button_message='Submit' />
         </Toolbar>
       </AppBar>
